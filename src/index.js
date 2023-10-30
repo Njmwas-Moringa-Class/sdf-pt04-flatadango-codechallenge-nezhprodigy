12af0e5d92bff26d21998d652d46f7785fb64567
alex <alex.musyoki@student.moringaschool.com>
2023-10-30 18:12:30 +0300

    Final

--- src/index.js
+++ src/index.js
@@ -1,77 +1,65 @@
 // Your code here
-const filmsList = document.querySelector("#films");
-const movieDetails = document.querySelector("#showing");
+const movieDetails = document.getElementById("showing");
 
-fetch("http://localhost:3000/films")
- .then((response) => response.json())
- .then((data) => {
-  data.forEach((film) => {
-   let filmHTML = `
-    <li class="film item">
-     <a href="#" data-id="${film.id}">${film.title}</a>
-     <button class="ui orange button buy-ticket">Buy Ticket</button>
-    </li>
-   `;
-   filmsList.insertAdjacentHTML("beforeend", filmHTML);
-  });
-
-    const availableTickets = data[0].capacity - data[0].tickets_sold;
-    const movieHTML = `
-    <div id="title" class="title">${data[0].title}</div>
-    <div id="runtime" class="meta">${data[0].runtime} minutes</div>
-    <img src="${data[0].poster}" alt="${data[0].title} poster" />
-    <div class="content">
-     <div class="description">
-      <div id="film-info">${data[0].description}</div>
-      <span id="showtime" class="ui label">${data[0].showtime}</span>
-      <span id="ticket-num">${availableTickets}</span> remaining tickets
-     </div>
-     <button id="buy-ticket" class="ui orange button">Buy Ticket</button>
+   // First Movie Details
+fetch('http://localhost:3000/films/1')
+.then(response => response.json())
+.then(data => {
+  const availableTickets = data.capacity - data.tickets_sold
+  const movieHTML = `
+  <div id ="title" class="title">${data.title}</div>
+  <div id ="runtime" class ="meta">${data.runtime}minutes</div>
+  <img src="${data.poster}" alt ="${data.title} poster"/>
+  <div class ="content">
+    <div class ="description">
+      <div id ="film-info">${data.description}</div>
+      <span id ="showtime" class ="ui label">${data.showtime}</span>
+      <span id ="ticket-num">${availableTickets}</span> remaining tickets
     </div>
+    <button id ="buy-ticket" class ="ui orange button">Buy Ticket</button>
+  </div> 
     `;
-    movieDetails.innerHTML = movieHTML;
-  });
+    movieDetails.innerHTML = movieHTML
+})
 
-filmsList.addEventListener('click', (e) => {
-  if (e.target.tagName === 'A') {
-    let filmId = e.target.dataset.id;
+// List of movies in the menu
+const filmsList = document.getElementById("films")
+fetch('http://localhost:3000/films')
+.then(response => response.json())
+.then(data => {
+  data.forEach(movie => {
+    const li = document.createElement('li');
+    li.classList.add('film', 'item');
+    li.textContent = movie.title;
+    li.addEventListener('click', () => {
+      const availableTickets = movie.capacity - movie.tickets_sold
+      const movieHTML = `
+      <div id = "title" class = "title">${movie.title}</div>
+      <div id = "runtime" class = "meta">${movie.runtime}minutes</div>
+      <img src = "${movie.poster}" alt = "${movie.title} poster"/>
+      <div class = "content">
+      <div class = "description">
+      <div id = "film-info">${movie.description}</div>
+      <span id = "showtime" class= "ui label">${movie.showtime}</span>
+      <span id = "">${availableTickets}</span> remaining tickets
+      </div>
+      <button id = "buy-ticket" class = "ui orange button">Buy Ticket</button>
+      </div>
+      `;
+      movieDetails.innerHTML = movieHTML;
 
-    fetch(`http://localhost:3000/films/${filmId}`)
-    .then((response) => response.json())
-    .then((data) => {
-
- const availableTickets = data.capacity - data.tickets_sold;
-     let movieHTML = `
-<div id="title" class="title”>${data.title}</div>
-<div id="runtime" class="meta”>${data.runtime} minuti</div>
-<img src="${data.poster}" alt="${data.title} poster" />
-<div class="content">
-  <div class="description">
-    <div id="film-info">${data.description}</div>
-    <span id="showtime" class="ui label”>${data.showtime}</span>
-    <span id="ticket-num">${availableTickets}</span> remaining tickets
-  </div>
-<button id="buy-ticket" class="ui orange button">Buy Ticket</button>
-</div>
-`;
-movieDetails.innerHTML = movieHTML;
-console.log(movieDetails)
- });
-}
-});
-
-document.getElementById('buy-ticket').addEventListener('click', () => {
-  let filmInfo = document.getElementById('film-info').dataset.id
-  
-    fetch(`http://localhost:3000/films/${filmInfo}`, {
-      method: 'POST'
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      let ticketNumber = document.getElementById('ticket-num');
-      let remainingTickets = data.capacity - data.tickets_sold;
-      ticketNumber.innerHTML = `${remainingTickets} remaining tickets`;
+      // Buying ticket button 
+      const buyTicket = document.getElementById("buy-ticket");
+buyTicket.addEventListener("click", () => {
+  if (availableTickets > 0) {
+    const newTickets = availableTickets - 1
+    availableTickets.textContent = newTickets
+    alert('You have a successfully bought a ticket!')
+  } else {
+    alert('SOLD OUT!')
+  }
+})
     });
-    // Buy ticket button
-  
+    filmsList.appendChild(li);
+  });
 });
