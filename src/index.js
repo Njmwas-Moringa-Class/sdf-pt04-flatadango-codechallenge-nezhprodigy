a2248fae66a8160805976e8fa2e0ff99d0deb9a7
alex <alex.musyoki@student.moringaschool.com>
2023-11-07 10:47:09 +0300

    updated JS

--- src/index.js
+++ src/index.js
@@ -1,38 +1,20 @@
 // Your code here
 const movieDetails = document.getElementById("showing");
+filmsList = document.getElementById("films");
+buyButton = document.getElementById("buy-ticket");
+ticketCount = parseInt(document.getElementById('ticket-num').textContent);
 
 // Retrieve details of the movie
-fetch('http://localhost:3000/films/1')
- .then(response => response.json())
- .then(data => {
-  const remainingTickets = data.capacity - data.tickets_sold;
-  const movieHTML = `
-   <div id="title" class="title">${data.title}</div>
-   <div id="runtime" class="meta">${data.runtime} minutes</div>
-   <img src="${data.poster}" alt="${data.title} poster"/>
-   <div class="content">
-    <div class="description">
-     <div id="film info">${data.description}</div>
-     <span id="showtime" class="ui label">${data.showtime}</span>
-     <span id="ticket-num">${remainingTickets}</span> remaining tickets
-    </div>
-    <button id ="buy-ticket" class ="ui orange button">Buy Ticket</button>
-   </div> 
-  `;
-  // Display the movie details on the webpage
-   movieDetails.innerHTML = movieHTML;
- });
-
- const filmsList = document.getElementById("films");
+ 
 fetch('http://localhost:3000/films')
 .then(response => response.json())
 .then(data => {
   data.forEach(movie => {
+   const remainingTickets = movie.capacity - movie.tickets_sold
   const li = document.createElement('li');
   li.classList.add('film', 'item');
   li.textContent = movie.title;
   li.addEventListener('click', () => {
-   const remainingTickets = movie.capacity - movie.tickets_sold;
    const movieHTML = `
 <div id="title" class="title">${movie.title}</div>
 <div id="runtime" class="meta">${movie.runtime} minutes</div>
@@ -48,10 +30,8 @@ fetch('http://localhost:3000/films')
 `;
 movieDetails.innerHTML= movieHTML;
 
-const buyButton = document.getElementById("buy-ticket");
 buyButton.addEventListener("click", () => {
- const ticketCount = parseInt(document.getElementById('ticket-num').textContent);
-
+ 
  if (ticketCount > 0) {
   const newTicketCount = ticketCount - 1;
   ticketCount.textContent = newTicketCount;
