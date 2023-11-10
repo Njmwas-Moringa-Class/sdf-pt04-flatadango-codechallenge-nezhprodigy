45d0590b3ee31523c04c721c97b4b21fa1a9047d
alex <alex.musyoki@student.moringaschool.com>
2023-11-11 00:28:56 +0300

    incomplete buy-buton

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 const movieDetails = document.getElementById("showing");
 filmsList = document.getElementById("films");
 buyButton = document.getElementById("buy-ticket");
-ticketCount = parseInt(document.getElementById('ticket-num').textContent);
 soldOutFilm = document.querySelector('.film.item');
+const ticketNum = document.getElementById('ticket-num');
 
 // Display first movie details
 fetch('http://localhost:3000/films/1')
@@ -27,7 +27,6 @@ fetch('http://localhost:3000/films/1')
 })
 
 // Retrieve details of the movies
- 
 fetch('http://localhost:3000/films')
 .then(response => response.json())
 .then(data => {
@@ -38,34 +37,33 @@ fetch('http://localhost:3000/films')
   li.textContent = movie.title;
   li.addEventListener('click', () => {
    const movieHTML = `
-<div id="title" class="title">${movie.title}</div>
-<div id="runtime" class="meta">${movie.runtime} minutes</div>
-<img src="${movie.poster}" alt="${movie.title} poster"/>
-<div class="content">
- <div class="description">
-   <div id="film info">${movie.description}</div>
-   <span id="showtime" class= "ui label">${movie.showtime}</span>
-   <span id="ticket-num">${remainingTickets}</span> remaining tickets
- </div>
- <button id ="buy-ticket" class ="ui orange button">Buy Ticket</button>
-</div>
-`;
-movieDetails.innerHTML= movieHTML;
+    <div id="title" class="title">${movie.title}</div>
+    <div id="runtime" class="meta">${movie.runtime} minutes</div>
+    <img src="${movie.poster}" alt="${movie.title} poster"/>
+    <div class="content">
+      <div class="description">
+       <div id="film info">${movie.description}</div>
+        <span id="showtime" class= "ui label">${movie.showtime}</span>
+        <span id="ticket-num">${remainingTickets}</span> remaining tickets
+      </div>
+      <button id ="buy-ticket" class ="ui orange button">Buy Ticket</button>
+    </div>`;
 
-buyButton.addEventListener("click", () => {
- 
- if (ticketCount > 0) {
-  const newTicketCount = ticketCount - 1;
-  ticketCount.textContent = newTicketCount;
-  alert('Congratulations! You have successfully purchased a ticket!');
- } else {
-  buyButton.textContent = 'SOLD OUT!';
-
-  soldOutFilm.classList.add('SOLD OUT!');
-  alert('Oops looks like this showing is already sold out!');
- }
+    movieDetails.innerHTML= movieHTML;
+  });
+  filmsList.appendChild(li);
 });
-    });
-    filmsList.appendChild(li);
-  })
-})
+});
+
+// Buying movie tickets
+buyButton.addEventListener('click', () => {
+  if (ticketNum > 0) {
+    const newTicketNum = ticketNum - 1;
+    ticketNum.textValue = newTicketNum;
+    alert('Congratulations! You have successfully purchased a ticket!');
+  } else {
+    buyButton.textContent = 'SOLD OUT!';
+    soldOutFilm.classList.add('SOLD OUT!');
+    alert('Oops looks like this showing is already sold out!');
+  }
+});
